93e95330d39bf6f9324e84dfeeeba8add95d1d84
Shay Goldin <goldinshay@gmail.com>
2025-10-29 11:24:05 +0100

    Initial commit: Spring Boot scaffold for E-rrig

---
+++ src/main/java/org/errig/ERrigApplication.java
@@ -0,0 +1,13 @@
+package org.errig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERrigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERrigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/errig/ERrigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.errig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERrigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
